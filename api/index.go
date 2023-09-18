a8af704032877a8440fdcc9f794c060ddba07daa
winai-pgm-itsystem <winai.k@itsystem-igt.com>
2023-09-18 21:29:50 +0700

    fix bug

--- api/index.go
+++ api/index.go
@@ -1,1 +1,15 @@
-package api
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
+
+}
