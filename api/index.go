0253cbf7b1e3324ae263c6fab5b56596945b62e9
winai-pgm-itsystem <winai.k@itsystem-igt.com>
2023-11-07 15:42:21 +0700

    remove port

--- api/index.go
+++ api/index.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"os"
-
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -17,10 +15,10 @@ func main() {
 		return c.SendString("Hello, World route by test 👋!")
 	})
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "8080"
+	// }
 
-	app.Listen(":" + port)
+	app.Listen(":")
 }
