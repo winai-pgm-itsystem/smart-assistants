29f4eb979558b195cff742ba022d3a03ef2facc0
winai-pgm-itsystem <winai.k@itsystem-igt.com>
2023-11-07 16:34:42 +0700

    update v1

--- api/index.go
+++ api/index.go
@@ -3,8 +3,17 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	fmt.Fprintf(w, "<h1>You SUCCESS DEPLOY GOLANG page 1</h1>")
+}
+
+func Main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", Handler).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
 }
