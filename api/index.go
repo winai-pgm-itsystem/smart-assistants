a06a2d45783ca2715d21354069bdbb06e2ddd901
winai-pgm-itsystem <winai.k@itsystem-igt.com>
2023-11-08 15:28:02 +0700

    update 22

--- api/index.go
+++ api/index.go
@@ -1,10 +1,40 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+var (
+	app *gin.Engine
+)
+
+func myRoute(r *gin.RouterGroup) {
+	r.GET("/user", func(c *gin.Context) {
+		c.JSON(http.StatusOK, map[string]any{
+			"userId":   "U001",
+			"userName": "john doe",
+		})
+	})
+}
+
+func helthCheck(r *gin.RouterGroup) {
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, map[string]any{
+			"app":     "smart-assistants",
+			"version": "v0.0.1",
+		})
+	})
+}
+func init() {
+	app = gin.New()
+	r := app.Group("/api")
+	myRoute(r)
+	helthCheck(r)
+
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	app.ServeHTTP(w, r)
 }
