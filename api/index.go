a417475cd45da66b97dcec0dbdf5e353798a352d
winai-pgm-itsystem <winai.k@itsystem-igt.com>
2023-11-09 22:10:30 +0700

    setup

--- api/index.go
+++ api/index.go
@@ -1,3 +1,42 @@
+// package handler
+
+// import (
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// var (
+// 	app *gin.Engine
+// )
+
+// func HelthCheck(r *gin.RouterGroup) {
+// 	r.GET("/", func(c *gin.Context) {
+
+// 		c.JSON(http.StatusOK, map[string]any{
+// 			"app":     "smart-assistants",
+// 			"version": "v0.0.1",
+// 		})
+// 	})
+// }
+
+// func main() {
+// 	app = gin.New()
+// 	router := app.Group("/api")
+// 	HelthCheck(router)
+
+// 	//secret
+// 	getSecret(router)
+
+// 	//user
+// 	postWebHook(router)
+
+// }
+
+// func Handler(w http.ResponseWriter, r *http.Request) {
+// 	app.ServeHTTP(w, r)
+// }
+
 package handler
 
 import (
@@ -6,33 +45,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	app *gin.Engine
+const (
+	StatusBadRequest = http.StatusBadRequest
+	StatusOK         = http.StatusOK
 )
 
-func helthCheck(r *gin.RouterGroup) {
-	r.GET("/", func(c *gin.Context) {
-
-		c.JSON(http.StatusOK, map[string]any{
-			"app":     "smart-assistants",
-			"version": "v0.0.1",
-		})
+func healthCheck(c *gin.Context) {
+	c.JSON(StatusOK, gin.H{
+		"message": "server running..",
 	})
 }
 
-func init() {
-	app = gin.New()
-	router := app.Group("/api")
-	helthCheck(router)
+func main() {
+	router := gin.Default()
+	router.GET("/", healthCheck)
 
-	//secret
-	getSecret(router)
+	router.Run()
 
-	//user
-	postWebHook(router)
-
-}
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-	app.ServeHTTP(w, r)
 }
