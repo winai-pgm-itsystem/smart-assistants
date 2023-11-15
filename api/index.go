e319b05e6433c8c2a6417b805fee04c0741fed60
winai-pgm-itsystem <winai.k@itsystem-igt.com>
2023-11-15 13:33:59 +0700

    update remove smell code

--- api/index.go
+++ api/index.go
@@ -1,23 +1,42 @@
-package api
+package handler
 
 import (
 	"net/http"
 
-	"github.com/labstack/echo/v4"
+	"github.com/gin-gonic/gin"
 )
 
-var srv http.Handler
+var app *gin.Engine
 
-func main() {
+func HealthCheck(r *gin.RouterGroup) {
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"app":     "smart-assistants",
+			"version": "v0.0.1",
+		})
 
-	e := echo.New()
-	e.GET("/", HealthCheck)
-	e.GET("/books", Book)
-	srv = e
-
-	e.Start(":8080")
+	})
 }
 
-func Hanlder(w http.ResponseWriter, r *http.Request) {
-	srv.ServeHTTP(w, r)
+// comment this when deploy to vercel
+// func SetupRoutes(app *gin.Engine) {
+// 	router := app.Group("/api")
+
+// 	HealthCheck(router)
+// 	CheckEnvirontmentTarget(router)
+// 	LineWebHookHandler(router)
+
+// }
+
+func init() {
+	app = gin.New()
+	router := app.Group("/api")
+	HealthCheck(router)
+	CheckEnvirontmentTarget(router)
+	LineWebHookHandler(router)
+
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	app.ServeHTTP(w, r)
 }
