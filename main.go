965c677ff957d23196f01a873014c1e40c6aed34
winai-pgm-itsystem <winai.k@itsystem-igt.com>
2025-01-11 16:15:15 +0700

    fixed reply text v2

--- main.go
+++ main.go
@@ -21,4 +21,17 @@ func main() {
 
 	// app.Run(":8080")
 
+	// message := "ขายอะไร"
+	// var replyText string
+
+	// if strings.Contains(message, "ขายอะไร") {
+	// 	replyText = "- กาแฟ และเครื่องดื่มค่ะ เช่น คาปูชิโน่ ลาเต้ อเมริกาโน่ ชาไทย"
+	// 	fmt.Println(replyText)
+	// } else if strings.Contains(message, "สวัสดี") {
+	// 	replyText = "สวัสดีค่ะ"
+	// 	fmt.Println(replyText)
+	// } else {
+	// 	replyText = "ขออภัยค่ะ ฉันไม่เข้าใจคำถาม กรุณาถามคำถามใหม่ค่ะ"
+	// 	fmt.Println(replyText)
+	// }
 }
