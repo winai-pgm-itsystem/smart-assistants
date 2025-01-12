f72c2a87f59d81dd4480c5de258817db0d1be669
winai-pgm-itsystem <winai.k@itsystem-igt.com>
2025-01-12 12:42:19 +0700

    reflect returned base code

--- handlers/handlerMessageEvent.go
+++ handlers/handlerMessageEvent.go
@@ -1,98 +1,88 @@
 package handlers
 
 import (
+	"fmt"
+	"os"
+	"smart-assistants/databases"
 	"smart-assistants/entities"
 	replymessages "smart-assistants/replyMessages"
-	"strings"
+	"smart-assistants/utils"
+	"strconv"
 )
 
 func HandleMessageEvent(webHookEvent *entities.WebHookEvent) {
 	message := webHookEvent.Events[0].Message.Text
 	replyToken := webHookEvent.Events[0].ReplyToken
-	// usserId := webHookEvent.Events[0].Source.UserID
+	usserId := webHookEvent.Events[0].Source.UserID
+
+	isMacherExpenseMode, expnenses := utils.MacherExpenseMode(message)
 	//
-	// isMacherExpenseMode, expnenses := utils.MacherExpenseMode(message)
-	// //
-	// ppqrAmount, macherPPQR := utils.MacherPPQRMode(message)
+	ppqrAmount, macherPPQR := utils.MacherPPQRMode(message)
 
-	// if usserId != os.Getenv("USER_ID") {
-	// 	replymessages.SendTextMessage(replyToken, "user id is invalid.")
-	// 	fmt.Println(" user id is invalid.")
+	if usserId != os.Getenv("USER_ID") {
+		replymessages.SendTextMessage(replyToken, "user id is invalid.")
+		fmt.Println(" user id is invalid.")
 
-	// } else if message == "test" {
-	// 	fmt.Println("ok message to new feature")
+	} else if message == "test" {
+		fmt.Println("ok message to new feature")
 
-	// } else if isMacherExpenseMode {
-	// 	category := entities.ExpenseCategories[expnenses[3]]
-	// 	remark := expnenses[4]
-	// 	amount, err := strconv.ParseFloat(expnenses[1], 32)
-	// 	if err != nil {
-	// 		replymessages.SendTextMessage(replyToken, fmt.Sprintf("amount is a  %s", err))
-	// 		return
-	// 	}
-	// 	id, recordErr := databases.AddExpenseHandler(category, float32(amount), remark)
-	// 	if recordErr != nil {
-	// 		replymessages.SendTextMessage(replyToken, recordErr.Error())
-	// 	}
-	// 	var currentTotalPerMonth float64
-	// 	var currentTotalPerDate float64
-	// 	if id != "" {
-	// 		month := utils.GetCurrentMonth()
-	// 		today := utils.GetCurrentDate()
-	// 		monthExpense, err := databases.GetExpenseHandler(month, "YYYY-MM")
-	// 		if err != nil {
-	// 			replymessages.SendTextMessage(replyToken, err.Error())
-	// 		}
-	// 		for i := 0; i < len(monthExpense.Records); i++ {
-	// 			if today == monthExpense.Records[i].Fields.Date {
-	// 				amountPerDate := monthExpense.Records[i].Fields.Amount
-	// 				currentTotalPerDate += float64(amountPerDate)
-	// 			}
-	// 			amountPerMonth := monthExpense.Records[i].Fields.Amount
-	// 			currentTotalPerMonth += float64(amountPerMonth)
-	// 		}
+	} else if isMacherExpenseMode {
+		category := entities.ExpenseCategories[expnenses[3]]
+		remark := expnenses[4]
+		amount, err := strconv.ParseFloat(expnenses[1], 32)
+		if err != nil {
+			replymessages.SendTextMessage(replyToken, fmt.Sprintf("amount is a  %s", err))
+			return
+		}
+		id, recordErr := databases.AddExpenseHandler(category, float32(amount), remark)
+		if recordErr != nil {
+			replymessages.SendTextMessage(replyToken, recordErr.Error())
+		}
+		var currentTotalPerMonth float64
+		var currentTotalPerDate float64
+		if id != "" {
+			month := utils.GetCurrentMonth()
+			today := utils.GetCurrentDate()
+			monthExpense, err := databases.GetExpenseHandler(month, "YYYY-MM")
+			if err != nil {
+				replymessages.SendTextMessage(replyToken, err.Error())
+			}
+			for i := 0; i < len(monthExpense.Records); i++ {
+				if today == monthExpense.Records[i].Fields.Date {
+					amountPerDate := monthExpense.Records[i].Fields.Amount
+					currentTotalPerDate += float64(amountPerDate)
+				}
+				amountPerMonth := monthExpense.Records[i].Fields.Amount
+				currentTotalPerMonth += float64(amountPerMonth)
+			}
 
-	// 	}
-	// 	limitPerMonth, err := strconv.ParseFloat(os.Getenv("LIMIT_EXPENSE_PER_MONTH"), 64)
-	// 	if err != nil {
-	// 		replymessages.SendTextMessage(replyToken, fmt.Sprintf("limit per month is a  %s", err))
-	// 		return
-	// 	}
+		}
+		limitPerMonth, err := strconv.ParseFloat(os.Getenv("LIMIT_EXPENSE_PER_MONTH"), 64)
+		if err != nil {
+			replymessages.SendTextMessage(replyToken, fmt.Sprintf("limit per month is a  %s", err))
+			return
+		}
 
-	// 	replymessages.SendFlexExpenseMessage(utils.GetSymbolCurrentcyFormat(amount), category, utils.GetSymbolCurrentcyFormat(currentTotalPerDate), utils.GetSymbolCurrentcyFormat(limitPerMonth-currentTotalPerMonth), utils.GetToday())
+		replymessages.SendFlexExpenseMessage(utils.GetSymbolCurrentcyFormat(amount), category, utils.GetSymbolCurrentcyFormat(currentTotalPerDate), utils.GetSymbolCurrentcyFormat(limitPerMonth-currentTotalPerMonth), utils.GetToday())
 
-	// } else if macherPPQR {
+	} else if macherPPQR {
 
-	// if ppqrAmount > 0 {
-	// 	replymessages.SendFlexPPQRMessage(utils.GetSymbolCurrentcyFormat(ppqrAmount), ppqrAmount)
+		if ppqrAmount > 0 {
+			replymessages.SendFlexPPQRMessage(utils.GetSymbolCurrentcyFormat(ppqrAmount), ppqrAmount)
 
-	// } else {
-	// 		replymessages.SendTextMessage(replyToken, "ppqr not generated.")
+		} else {
+			replymessages.SendTextMessage(replyToken, "ppqr not generated.")
 
-	// 	}
+		}
 
-	// }
-
-	// else {
-	// 	result, err := utils.AIGenearateText(message)
-	// 	if err != nil {
-	// 		replymessages.SendTextMessage(replyToken, fmt.Sprintf("message %s not macher.", message))
-	// 	}
-
-	// 	replymessages.SendTextMessage(replyToken, fmt.Sprintf(`%s`, result))
-
-	// }
-	var replyText string
-
-	if strings.Contains(message, "ขายอะไร") {
-		replyText = `- กาแฟ และเครื่องดื่มค่ะ เช่น\n- คาปูชิโน่\n- ลาเต้\n- อเมริกาโน่\n - ชาไทย`
-		replymessages.SendTextMessage(replyToken, replyText)
-	} else if strings.Contains(message, "สวัสดี") {
-		replyText = "สวัสดีค่ะ"
-		replymessages.SendTextMessage(replyToken, replyText)
 	} else {
-		replyText = "ขออภัยค่ะ ฉันไม่เข้าใจคำถาม กรุณาถามคำถามใหม่ค่ะ"
-		replymessages.SendTextMessage(replyToken, replyText)
+		result, err := utils.AIGenearateText(message)
+		if err != nil {
+			replymessages.SendTextMessage(replyToken, fmt.Sprintf("message %s not macher.", message))
+		}
+
+		replymessages.SendTextMessage(replyToken, fmt.Sprintf(`%s`, result))
+
 	}
 
 }
--- main.go
+++ main.go
@@ -1,96 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"strings"
+// import (
+// 	"log"
 
-	tf "github.com/galeone/tensorflow/tensorflow/go"
-)
+// 	handler "smart-assistants/api"
+
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/joho/godotenv"
+// )
 
 func main() {
-	// โหลดโมเดลจาก SavedModel directory
-	model, err := tf.LoadSavedModel("BiLSTM_sentiment_model", []string{"serve"}, nil)
-	if err != nil {
-		log.Fatalf("Error loading model: %v", err)
-	}
-	defer model.Session.Close()
+	// comment this when deploy to vercel
+	// err := godotenv.Load("prod.env")
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
-	// ตัวอย่างข้อความ input ที่จะทำการทำนาย
-	input := "This is a great product!"
+	// app := gin.New()
+	// handler.SetupRoutes(app)
 
-	// Preprocess ข้อมูล (tokenization, padding)
-	xTest := manualTokenization(input)
-	xTest = padSequence(xTest, 100) // max_length = 100
+	// app.Run(":8080")
 
-	// สร้าง Tensor จากข้อมูล input
-	tensor, err := tf.NewTensor(xTest)
-	if err != nil {
-		log.Fatalf("Error creating tensor: %v", err)
-	}
-
-	// รันโมเดลเพื่อทำการทำนาย
-	result, err := model.Session.Run(
-		map[tf.Output]*tf.Tensor{
-			model.Graph.Operation("serving_default_input_1").Output(0): tensor, // ปรับชื่อ input node ให้ตรง
-		},
-		[]tf.Output{
-			model.Graph.Operation("StatefulPartitionedCall").Output(0), // ปรับชื่อ output node ให้ตรง
-		},
-		nil,
-	)
-	if err != nil {
-		log.Fatalf("Error running the model: %v", err)
-	}
-
-	// ตรวจสอบผลลัพธ์
-	prediction := result[0].Value().([][]float32)[0][0]
-
-	// ตัดสินใจว่าผลลัพธ์เป็น positive หรือ negative
-	var response string
-	if prediction > 0.5 {
-		response = "positive"
-	} else {
-		response = "negative"
-	}
-
-	// แสดงผลลัพธ์
-	fmt.Printf("Prediction: %s\n", response)
-}
-
-// ฟังก์ชั่นเพื่อทำการ tokenization (แปลงข้อความเป็น token) ใน Go
-func manualTokenization(input string) []int {
-	// ตัวอย่างการแปลงข้อความเป็นตัวเลขโดยใช้ dictionary (ง่ายๆ)
-	dictionary := map[string]int{
-		"great":   1,
-		"product": 2,
-		"this":    3,
-		"is":      4,
-		"a":       5,
-	}
-
-	words := strings.Fields(input) // แยกคำจาก input
-	tokens := make([]int, len(words))
-
-	for i, word := range words {
-		// หากคำมีอยู่ใน dictionary ให้แปลงเป็น token
-		if token, exists := dictionary[word]; exists {
-			tokens[i] = token
-		} else {
-			tokens[i] = 0 // หากไม่มีใน dictionary, ให้เป็น 0
-		}
-	}
-	return tokens
-}
-
-// ฟังก์ชั่นเพื่อทำการ padding sequence ให้ยาวตาม max_length
-func padSequence(sequence []int, maxLength int) []int {
-	if len(sequence) >= maxLength {
-		return sequence[:maxLength]
-	}
-
-	// ถ้าความยาว sequence น้อยกว่า maxLength, ทำการ padding
-	paddedSequence := make([]int, maxLength)
-	copy(paddedSequence, sequence)
-	return paddedSequence
 }
